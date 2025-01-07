67d4b86b87fb02f1dc28046f1d61abf22319a6ca
Balaji Kamble <akashkamble965@gmail.com>
2025-01-07 15:49:38 +0530

    Login logic

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,33 +1,65 @@
 import userModels from "../models/userModels.js";
 
 export const registerController = async (req, res, next) => {
-  try {
-    const { name, email, password } = req.body;
-    //validate
-    if (!name) {
-      next("Name is required");
-    }
-    if (!email) {
-      next("Email is required");
-    }
-    if (!password) {
-      next("Password is required and greater than  6 characters");
-    }
-    const existingUser = await userModels.findOne({ email });
-    if (existingUser) {
-      next("Email already exists");
-    }
-    const user = await userModels.create({
-      name,
-      email,
-      password,
-    });
-    res.status(201).send({
-      message: "User Registered Successfully",
-      success: true,
-      user,
-    });
-  } catch (error) {
-    next(error);
+  const { name, email, password } = req.body;
+  //validate
+  if (!name) {
+    next("Name is required");
   }
+  if (!email) {
+    next("Email is required");
+  }
+  if (!password) {
+    next("Password is required and greater than  6 characters");
+  }
+  const existingUser = await userModels.findOne({ email });
+  if (existingUser) {
+    next("Email already exists");
+  }
+  const user = await userModels.create({
+    name,
+    email,
+    password,
+  });
+  //token
+  const token = user.createJWT();
+  res.status(201).send({
+    message: "User Registered Successfully",
+    success: true,
+    user: {
+      name: user.name,
+      lastName: user.lastName,
+      email: user.email,
+      location: user.location,
+    },
+    token,
+  });
+};
+
+export const loginController = async (req, res, next) => {
+  const { email, password } = req.body;
+  //validation
+  if (!email || !password) {
+    next("Email and Password are required");
+  }
+
+  //find user by email
+  const user = await userModels.findOne({ email }).select("+password");
+  if (!user) {
+    next("Invail email and password");
+  }
+  //compare password
+  const isMatch = await user.comparePassword(password);
+  if (isMatch) {
+    next("Invail email and password");
+  }
+  user.password = undefined;
+  //token
+  const token = user.createJWT();
+  res.status(200).json({
+    success: true,
+    message: "Login successfully",
+    user: {},
+    token,
+  });
 };
--- models/userModels.js
+++ models/userModels.js
@@ -1,6 +1,11 @@
 import mongoose from "mongoose";
 import validator from "validator";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 //schema
+const jwtSecreteKey =
+  process.env.JWT_SECRET || "ABCDEFHHIJKLMNO@#$%&1234567890";
+console.log("JWTSecretKey", jwtSecreteKey);
 const userSchema = new mongoose.Schema(
   {
     name: {
@@ -20,6 +25,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: [true, "Password is Required"],
       minLength: [6, "Password must be at least 6"],
+      select: true,
     },
     location: {
       type: String,
@@ -28,5 +34,22 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+//middlewares
+userSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+//compare Password
 
+userSchema.methods.comparePassword = async function (userPassword) {
+  const isMatch = await bcrypt.compare(userPassword, this.password);
+  return isMatch;
+};
+
+//JSON WEBTOKEN
+userSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id }, jwtSecreteKey, {
+    expiresIn: "1d",
+  });
+};
 export default mongoose.model("User", userSchema);
